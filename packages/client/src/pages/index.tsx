e7623d5311d3aecde621c525b62e27e53288c811
Andrew Glago <andrewglago1@gmail.com>
2021-12-10 13:32:13 +0000

    landing

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,61 +1,11 @@
+import React from "react";
 import type { NextPage } from "next";
 import Image from "next/image";
-import { useState, useEffect, useCallback } from "react";
-import { ethers } from "ethers";
-import { Storage__factory } from "@/typechain";
-import { hasEthereum } from "@/utils";
 import Head from "next/head";
+import { motion } from "framer-motion";
 import WorldImage from "../assets/world.png";
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
 const Home: NextPage = () => {
-  const [currentStore, setCurrentStore] = useState("");
-  const [inputState, setInputState] = useState("");
-  const [status, setStatus] = useState<"loading..." | "complete">("complete");
-  async function requestAccount() {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-  }
-
-  const setStore = async () => {
-    if (hasEthereum()) {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const storageContract = Storage__factory.connect(contractAddress, signer);
-      const tx = await storageContract.set(inputState);
-      setStatus("loading...");
-      const receipt = await tx.wait();
-      if (receipt.status === 1) {
-        setCurrentStore(inputState);
-        setInputState("");
-      }
-      setStatus("complete");
-    }
-  };
-  async function fetchStore() {
-    if (hasEthereum()) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const storageContract = Storage__factory.connect(
-        contractAddress,
-        provider
-      );
-      try {
-        const data = await storageContract.retrieve();
-        setCurrentStore(data);
-      } catch (err) {
-        console.log("EfetchStorerror: ", err);
-      }
-    }
-  }
-  const memoizedFetchStore = useCallback(async () => {
-    let response = await fetchStore();
-  }, []);
-
-  useEffect(() => {
-    memoizedFetchStore();
-  }, [memoizedFetchStore]);
-
   return (
     <div>
       <Head>
@@ -81,16 +31,29 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className="">
-          <Image
-            src={WorldImage}
-            className="bg-white"
-            alt="Matrix World Globe"
+        <div className="p-5">
+          <AnimatedGlobe
+            animate={{
+              rotate: [0, 360],
+            }}
+            transition={{
+              duration: 200,
+              ease: "linear",
+              repeat: Infinity,
+            }}
           />
         </div>
       </div>
     </div>
   );
 };
+
+const AnimatedGlobe = motion(
+  React.forwardRef((props, ref) => (
+    <div ref={ref as any} {...props}>
+      <Image draggable="false" src={WorldImage} alt="Matrix World Globe" />
+    </div>
+  ))
+);
 
 export default Home;
