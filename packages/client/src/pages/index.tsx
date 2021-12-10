d9050de6d0f96e27de3cbfdd2ce3252881e67173
Andrew Glago <andrewglago1@gmail.com>
2021-12-10 03:10:10 +0000

    landing init

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { useState, useEffect, useCallback } from "react";
 import { ethers } from "ethers";
 import { Storage__factory } from "@/typechain";
 import { hasEthereum } from "@/utils";
 import Head from "next/head";
+import WorldImage from "../assets/world.png";
 
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
@@ -59,7 +61,34 @@ const Home: NextPage = () => {
       <Head>
         <title>Advent</title>
       </Head>
-      <div className="text-red-500">Yo yo</div>
+      <div className="flex flex-col-reverse md:flex-row p-4 max-w-screen-2xl justify-between m-auto">
+        <div className="md:w-1/2 flex flex-col justify-center gap-4 text-lg p-4">
+          <div className="flex flex-col justify-center gap-4 text-lg p-4">
+            <h1 className="text-2xl font-bold">
+              Discover and sell advertising spaces on MatrixWorld
+            </h1>
+            <p>
+              Advent is a marketplace for trading and listing ad spaces in
+              MatrixWorld.
+            </p>
+            <div className="flex gap-4">
+              <button className="px-8 py-2 bg-primaryGreen text-white rounded-md hover:bg-opacity-70 transition-all duration-200 ease-in-out">
+                Explore
+              </button>
+              <button className="px-8 py-2 bg-white border border-primaryGreen rounded-md transition-all duration-200 ease-in-out hover:shadow-lg">
+                Advertise
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <Image
+            src={WorldImage}
+            className="bg-white"
+            alt="Matrix World Globe"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryGreen: "hsl(119deg 53% 40%)",
+      },
+    },
   },
   variants: {
     extend: {},
